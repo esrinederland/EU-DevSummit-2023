7257bd464ce0a4ee8d37e6acc862de4827c3d2d0
MaartjeHoltslag <mcsholtslag@gmail.com>
2023-10-30 16:49:58 +0100

    CreateWebMap(s)

---
+++ Python Administration/07_CreateWebmap.py
@@ -0,0 +1,35 @@
+import arcgis
+from Settings import PortalUrl,ProfileName, AgolUrl, AgolProfileName, EarthquakeRenderer
+import os
+
+print("Getting GIS")
+gis = arcgis.GIS(AgolUrl, profile=AgolProfileName)
+print("Successfully logged into '{}' via the '{}' user".format(gis.properties.portalHostname,gis.properties.user.username))
+
+# CREATE WEBMAP
+print("Creating a webmap")
+wm = arcgis.mapping.WebMap()  # new web map
+
+wm.basemap = "streets-night-vector"
+
+layer = gis.content.get("5da9f6e8810741d7859491c32f1b46be").layers[0]
+                
+wm.add_layer(
+    layer,
+    options= {
+        "title": "Earthquakes",
+        "renderer": EarthquakeRenderer
+    })
+
+# SAVE THE WEBMAP
+webmap_item_properties = {
+    'title':'DevSummit Earthquakes with magnitude above 5.5 in 2023',
+    'snippet':'Map created using Python API',
+    'tags':['automation', 'python', "DevSummit2023"]
+    }
+
+print("Saving the webmap")
+new_wm_item = wm.save(webmap_item_properties, thumbnail=r'D:\Data\WebMap_Icon.jpg')
+print(f"Created item with id: {new_wm_item.id}")
+
+print("Script complete")
--- Python Administration/09_CreateWebmaps.py
+++ Python Administration/09_CreateWebmaps.py
@@ -1,14 +1,14 @@
 import arcgis
-from Settings import PortalUrl,ProfileName
+from Settings import PortalUrl,ProfileName, EarthquakeRenderer
 
 gis = arcgis.GIS(PortalUrl, profile=ProfileName)
 print("Successfully logged into '{}' via the '{}' user".format(gis.properties.portalHostname,gis.properties.user.username))
 
 #get featurelayer
-layer = gis.content.get("96cbbfd990844b78a3150313ea9971d6").layers[0]
+layer = gis.content.get("5da9f6e8810741d7859491c32f1b46be").layers[0]
 
 #get unique values
-uniqueValues = layer.get_unique_values("Gemeente")
+uniqueValues = layer.get_unique_values("year")
 
 print(f"Found: {len(uniqueValues)} unique values")
 uniqueValues.sort(reverse=True)
@@ -17,23 +17,25 @@ for unique in uniqueValues:
     counter += 1
     print(f"Creating webmap for {unique} ({counter}/{len(uniqueValues)}))")
 
-    sql_expression = f"Gemeente = '{unique}'"
+    sql_expression = f"year = {unique}"
 
     wm = arcgis.mapping.WebMap()  # new web map
 
-    wm.basemap = "dark-gray-vector"
+    wm.basemap = "streets-night-vector"
 
-    wm.add_layer(layer)  # add some layers
+    wm.add_layer(
+        layer,
+        options= {
+        "title": "Earthquakes",
+        "renderer": EarthquakeRenderer
+    })
 
     wm.layers[0].layerDefinition.definitionExpression = sql_expression
 
-    extent = layer.query(where=sql_expression,return_extent_only = True, out_sr=4326)
-
     # SAVE THE WEBMAP
-    webmap_item_properties = {'title':f'DevSummit Created Webmap for {unique}',
+    webmap_item_properties = {'title':f'Earthquakes with magnitude above 5.5 in {unique}',
                 'snippet':'Map created using Python API',
-                'tags':['automation', 'python', "DevSummit2023"],
-                'extent': extent['extent']}
+                'tags':['automation', 'python', "DevSummit2023"]}
 
     print("Saving the webmap")
     new_wm_item = wm.save(webmap_item_properties, thumbnail=r'D:\Data\WebMap_Icon.jpg')
