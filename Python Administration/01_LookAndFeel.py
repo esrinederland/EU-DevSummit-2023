64967efa6d71bbf532fb0c7a33da4ca67247f59b
MaartjeHoltslag <mcsholtslag@gmail.com>
2023-11-06 11:20:08 +0100

    Comments for LookAndFeel

--- Python Administration/01_LookAndFeel.py
+++ Python Administration/01_LookAndFeel.py
@@ -11,14 +11,16 @@ print("Getting GIS")
 gis = arcgis.GIS(PortalUrl, profile=ProfileName)
 print("Successfully logged into '{}' via the '{}' user".format(gis.properties.portalHostname,gis.properties.user.username)) 
 
+# GET UX OBJECT
 ux = gis.admin.ux
 
 # CHANGE HOMEPAGE TITLE
 newTitle = "DevTeam Server [European DevSummit 2023]"
 ux.homepage_settings.set_title(newTitle)
-print(f"Home page title updated to {newTitle}")
+print(f"Home page title updated to '{newTitle}'")
 
 # CHANGE HOMEPAGE BACKGROUND
+print("Changing background image")
 filename = Path(r'D:\Data\background.jpg')
 if filename.exists():
     ux.homepage_settings.set_background(
@@ -29,12 +31,15 @@ else:
 
 # CHANGE PORTAL NAME
 editDatTime = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-ux.name = f"Our Fabulous portal {editDatTime}"
+newName = f"Our Fabulous portal {editDatTime}"
+ux.name = newName
+print(f"Portal name updated to '{newName}'")
 
 # SET INFORMATINONAL BANNER
+print("Creating informational banner")
 ux.security_settings.set_informational_banner(
     text=f"Information: This Portal's look and feel was updated on {editDatTime}", 
-    bg_color="white", 
+    bg_color="#C0FFEE", 
     enabled=True)
 
 print("Script complete")
